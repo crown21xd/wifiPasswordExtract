2549d74f45898b41d8e25175e152dc12a52e4312
crown21xd <82134115+crown21xd@users.noreply.github.com>
2025-11-25 23:16:26 +0800

    cleans up mess after extracting The wifi password
    cleans up mess after extracting The wifi password

---
+++ directoryAutoDelete.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Change to the parent directory
+cd ..
+
+rem Remove the directory named wifiPasswordExtract
+rmdir /s /q wifiPasswordExtract
+
+echo Directory 'wifiPasswordExtract' has been removed.
